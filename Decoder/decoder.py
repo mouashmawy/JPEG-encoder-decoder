be7326ea057e5ab5406e6c98d269bc5b1d105ca7
Moustafa Ashamwy <mouashmawy@gmail.com>
2023-11-18 00:47:26 +0200

    cont init

---
+++ Decoder/decoder.py
@@ -0,0 +1,60 @@
+from numpy import *
+from DCT_decoder import *
+from huffman_decoder import *
+
+
+#describe your function
+def Huffman_deconding(array):
+
+
+    return #array 1xN
+
+
+
+#describe your function
+def decoding_one_length(array):
+
+
+    return #array 1x64
+
+
+#describe your function
+def inverse_linearing(array):
+
+
+
+    return #block 8x8
+
+
+
+
+#describe your function
+def dequantization(block):
+
+
+
+    return #dequantized block (8x8)
+
+
+#describe your function
+def IDCT(block8x8):
+
+    
+
+    return #inversed DCT block 8x8
+
+
+
+#describe your function
+def collect_image(array_of_blocks):
+
+
+
+
+    return #collected_image
+
+
+
+
+
+
--- Encoder/encoder.py
+++ Encoder/encoder.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from DCT_encoder import *
+from Huffman_encoder import *
 
 
 
