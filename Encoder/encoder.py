628e9736661b59a6d610c586d28de365d2fc4286
Moustafa Ashamwy <mouashmawy@gmail.com>
2023-11-17 16:58:06 +0200

    init

---
+++ Encoder/encoder.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+
+
+#describe your function
+def divide_image(image):
+
+
+
+
+
+    return #blocks 8x8
+
+
+
+#describe your function
+def DCT(block8x8):
+
+    
+
+    return #DCT block 8x8
+
+
+
+#describe your function
+def quantization(block):
+
+
+
+    return #quantized block (8x8)
+
+
+
+#describe your function
+def linearing(block):
+
+
+
+    return #array 1x64
+
+
+
+#describe your function
+def encoding_one_length(array):
+
+
+    return #array 1xN
+
+
+
+#describe your function
+def Huffman_enconding(array):
+
+
+    return #stream of 0s & 1s
+
+
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+from Encoder.encoder import *
+from Decoder.decoder import *
+
+
+
+def main():
+
+
+
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
